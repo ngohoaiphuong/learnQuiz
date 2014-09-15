1f86975f98aaf9c085e62f1c29dc4080f65b5877
vfa-phuongnh <vfa-phuongnh@gmail.com>
2014-09-15 19:10:23 +0700

    basic project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LearnQuiz::Application.config.secret_token = 'a8e10153f028d30c35d60eac0a265aa8d779f72ba428d7ddd9ed1c647ef91601217adf7a35a15395de36756b69a33a060282a76f8b027fd5fee38fb87c0075a4'
